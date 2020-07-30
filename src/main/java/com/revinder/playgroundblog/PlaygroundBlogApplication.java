b7b82f31d315937306986fb05f58e6f966fefa8a
Michael Kasprzyk <michal0kasprzyk@gmail.com>
2020-07-30 02:49:46 +0200

    initial structure

---
+++ src/main/java/com/revinder/playgroundblog/PlaygroundBlogApplication.java
@@ -0,0 +1,13 @@
+package com.revinder.playgroundblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaygroundBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaygroundBlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/revinder/playgroundblog/PlaygroundBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revinder.playgroundblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaygroundBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
