b94fd691ed5d87409c867f556db9713ae098e801
Michael Kasprzyk <michal0kasprzyk@gmail.com>
2020-08-01 00:50:58 +0200

    in memory user for debug only

--- src/main/java/com/revinder/playgroundblog/config/WebSecurityConfig.java
+++ src/main/java/com/revinder/playgroundblog/config/WebSecurityConfig.java
@@ -1,10 +1,16 @@
 package com.revinder.playgroundblog.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
@@ -40,5 +46,25 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
         http.csrf().ignoringAntMatchers("/api/login")
                 .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+    }
+
+
+    @Bean
+    public UserDetailsService userDetailsService()
+    {
+        String pwd = "pwd";
+        String username = "user";
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        String encodedPwd = passwordEncoder().encode(pwd);
+
+        manager.createUser(User.withUsername(username).password(pwd).roles("USER").build());
+        return manager;
+    }
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder()
+    {
+        return new BCryptPasswordEncoder();
     }
 }
