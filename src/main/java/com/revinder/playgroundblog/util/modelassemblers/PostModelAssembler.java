8224b8b318f763d8dc081c4562e53a3294a12645
Michael Kasprzyk <michal0kasprzyk@gmail.com>
2020-08-14 22:02:50 +0200

    + Added Post model assembler

---
+++ src/main/java/com/revinder/playgroundblog/util/modelassemblers/PostModelAssembler.java
@@ -0,0 +1,19 @@
+package com.revinder.playgroundblog.util.modelassemblers;
+
+import com.revinder.playgroundblog.controller.UserController;
+import com.revinder.playgroundblog.model.Post;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+public class PostModelAssembler implements RepresentationModelAssembler<Post, EntityModel<Post>> {
+
+    @Override
+    public EntityModel<Post> toModel(Post entity) {
+        return EntityModel.of(entity,
+                linkTo(methodOn(UserController.class).findById(entity.getId())).withSelfRel(),
+                linkTo(methodOn(UserController.class).findAll()).withRel("posts"));
+    }
+}
