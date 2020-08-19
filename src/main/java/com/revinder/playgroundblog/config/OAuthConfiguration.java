647bc3298c83d7cf35ee5e85e63721cffda0c706
Michael Kasprzyk <michal0kasprzyk@gmail.com>
2020-08-19 16:55:17 +0200

    + Removed old security config

--- src/main/java/com/revinder/playgroundblog/config/OAuthConfiguration.java
+++
@@ -1,8 +0,0 @@
-package com.revinder.playgroundblog.config;
-
-import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
-
-public class OAuthConfiguration extends AuthorizationServerConfigurerAdapter {
-
-    // TODO: Keycloak configuration and Oauth2
-}
