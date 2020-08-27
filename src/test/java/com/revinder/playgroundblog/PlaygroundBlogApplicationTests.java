742f73ad41d1244c1af420d24ee61e9298ab79d4
Michael Kasprzyk <michal0kasprzyk@gmail.com>
2020-08-28 00:32:59 +0200

    + fixed small bug. todo write tests

--- src/test/java/com/revinder/playgroundblog/PlaygroundBlogApplicationTests.java
+++ src/test/java/com/revinder/playgroundblog/PlaygroundBlogApplicationTests.java
@@ -1,13 +1,8 @@
 package com.revinder.playgroundblog;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class PlaygroundBlogApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
